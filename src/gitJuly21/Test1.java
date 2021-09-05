1526efe9eb67221c2f4dc1ad686f168331facbdb
Devendra Chandankhede <www.devendrachandankhede@gmail.com>
2021-09-05 10:04:06 +0530

    GitProject upload

---
+++ src/gitJuly21/Test1.java
@@ -0,0 +1,8 @@
+package gitJuly21;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
---
+++ src/gitJuly21/Test_2.java
@@ -0,0 +1,7 @@
+package gitJuly21;
+
+public class Test_2 {
+	public static void main(String[] args) {
+		System.out.println("Hey");
+	}
+}
